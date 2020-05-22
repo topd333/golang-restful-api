4028d1a66b27c4dce7ff53f49fafb3977a272170
topd333 <genius.snow0405@gmail.com>
2020-05-22 06:18:32 +0300

    fix route

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
     router.HandleFunc("/api/user/new", controllers.CreateAccount).Methods("POST")
     router.HandleFunc("/api/user/login", controllers.Authenticate).Methods("POST")
-    outer.HandleFunc("/api/contacts/new", controllers.CreateContact).Methods("POST")
+    router.HandleFunc("/api/contacts/new", controllers.CreateContact).Methods("POST")
     router.HandleFunc("/api/me/contacts", controllers.GetContactsFor).Methods("GET")
 
     router.Use(app.JwtAuthentication) //attach JWT auth middleware
